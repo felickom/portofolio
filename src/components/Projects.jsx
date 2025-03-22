d3b70545c58be320e688889774058763c195079f
Felic Komarno <115445105+felickom@users.noreply.github.com>
2025-03-22 16:46:58 +0700

    Update
    - Update Portofolio Projects, Navbar, Home, About, Experiences, dan Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,7 +31,7 @@ const Projects = (props) => {
       .then((res) => setData(res))
       .catch((err) => err);
   }, []);
-  const numberOfItems = showMore && data ? data.length : 6;
+  const numberOfItems = showMore && data ? data.length : 3;
   return (
     <>
       <Header title={header} />
@@ -47,7 +47,7 @@ const Projects = (props) => {
                 ))}
               </Row>
 
-              {!showMore
+              {data.projects.length > 3 && !showMore
                 && (
                 <Button
                   style={styles.showMoreStyle}
